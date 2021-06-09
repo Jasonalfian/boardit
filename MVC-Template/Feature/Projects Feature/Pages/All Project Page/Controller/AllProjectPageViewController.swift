3e1b20d86ed122fd76c390c7e0650e60e543b2cf
Gilbert Nicholas <gilbertnicholas09@gmail.com>
2021-06-10 01:46:19 +0700

    INITIAL FILE FIX BET FIX

--- MVC-Template/Feature/Projects Feature/Pages/All Project Page/Controller/AllProjectPageViewController.swift
+++ MVC-Template/Feature/Projects Feature/Pages/All Project Page/Controller/AllProjectPageViewController.swift
@@ -11,11 +11,23 @@ class AllProjectPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    @IBAction func allSceneButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "allSceneSegue", sender: self)
+    }
+    
     /*
     // MARK: - Navigation
 
