88df7c9b4c5d3efa21bf7b07ac3c1d31c07c528b
Andy Lukito <andylukito97@gmail.com>
2021-06-11 01:13:21 +0700

    Prepare Variables

--- MVC-Template/Feature/Presentation Feature/Pages/Presentation Page/Controller/PresentationPageViewController.swift
+++ MVC-Template/Feature/Presentation Feature/Pages/Presentation Page/Controller/PresentationPageViewController.swift
@@ -9,22 +9,17 @@ import UIKit
 
 class PresentationPageViewController: UIViewController {
 
+    @IBOutlet weak var descriptionText : UILabel!
+    @IBOutlet weak var angleText : UILabel!
+    @IBOutlet weak var shotText : UILabel!
+    @IBOutlet weak var movementText : UILabel!
+    
+    @IBOutlet weak var counterSceneText : UILabel!
+    @IBOutlet weak var thumbnailImage : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("a")
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
