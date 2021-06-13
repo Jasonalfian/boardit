2385e2c6decd367a2197718efe828e65351f0abe
Andy Lukito <andylukito97@gmail.com>
2021-06-14 03:02:10 +0700

    Sections done (tinggal select row)

--- MVC-Template/Feature/Presentation Feature/Pages/Presentation Page/Controller/PresentationPageViewController.swift
+++ MVC-Template/Feature/Presentation Feature/Pages/Presentation Page/Controller/PresentationPageViewController.swift
@@ -40,22 +40,36 @@ class PresentationPageViewController: UIViewController {
 }
 
 class SceneListController : UITableViewController{
-    var items = ["A", "B", "C"]
+    
+    var totalScenes = [ [1,2,3], [1,2,3,4], [1,2] ]
+    let otherScript = PresentationPageViewController()
+    
     let darkColour = #colorLiteral(red: 0.1450980392, green: 0.1450980392, blue: 0.1450980392, alpha: 1)
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView = UITableView.init(frame: .zero, style: .grouped)
         tableView.backgroundColor = darkColour
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        // Do any additional setup after loading the view.
+        
+        //Get Core Data
+        totalScenes = [ [1,2,3], [1,2,3,4], [1,2] ]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return totalScenes.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return totalScenes[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Scene \(section)"
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = "Scene \(indexPath.section+1).\(indexPath.row+1)"
         cell.textLabel?.textColor = .white
         cell.backgroundColor = darkColour
         return cell
@@ -63,7 +77,8 @@ class SceneListController : UITableViewController{
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
+        otherScript.counterSceneText?.text = "A"
+//        otherScript.counterSceneText?.text = "\(indexPath.row)/\(indexPath.section)"
         //Change right side content
     }
 }
